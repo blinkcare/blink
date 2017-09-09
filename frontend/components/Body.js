1ffe9c640001f20050a109a18e4da7a88a6c97de
Lachlan Campbell <lachlan.campbell@icloud.com>
2017-09-09 07:44:50 -0400

    Working on fetching

--- frontend/components/Body.js
+++ frontend/components/Body.js
@@ -1,24 +1,54 @@
 import React, { Component } from 'react'
 import { Box, Flex, Text } from 'rebass'
+import { isEmpty } from 'lodash'
+import fetch from 'unfetch'
 import Letter from './Letter'
 import Typing from './Typing'
+
+const ENDPOINT = ''
 
 class Body extends Component {
   constructor() {
     super()
-    this.state = {}
+    this.state = {
+      letters: ['H', 'e', 'l', 'l', 'o', ' '],
+      typing: true
+    }
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.fetchData()
+    }, 1000)
+  }
+
+  fetchData() {
+    const head = {
+      method: 'GET',
+      mode: 'cors'
+    }
+    fetch(ENDPOINT, head)
+      .then(res => {
+        return res.json()
+      })
+      .then(json => {
+        if (isEmpty(json.new)) {
+          this.setState({ typing: true })
+        } else {
+          const { letters } = this.state
+          letters.push(json.new)
+          this.setState({ letters, typing: false })
+        }
+      })
   }
 
   render() {
+    const { letters, typing } = this.state
     return (
       <Box>
         <Flex align="center">
-          <Letter>H</Letter>
-          <Letter>e</Letter>
-          <Letter>l</Letter>
-          <Letter>l</Letter>
-          <Letter>o</Letter>
-          <Typing ml={3} />
+          {letters.map(letter => <Letter>{letter}</Letter>)}
+          {typing && <Typing ml={3} />}
         </Flex>
       </Box>
     )
