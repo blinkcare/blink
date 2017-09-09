081aeb395d5c15686d56986c522228dd0e72458e
Lachlan Campbell <lachlan.campbell@icloud.com>
2017-09-09 14:04:36 -0400

    Center header

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -11,22 +11,8 @@ const Name = styled(Heading)`
   color: ${colors.grey};
 `
 
-const bulge = keyframes`
-  50% {
-    transform: scale(.75);
-  }
-`
-const Badge = styled.span`
-  display: inline-block;
-  width: 0.75rem;
-  height: 0.75rem;
-  border-radius: 100%;
-  background-color: ${props => (props.status ? colors.red : colors.grey)};
-  animation: ${bulge} 2s ease-in-out infinite;
-`
-
 const Header = ({ status }) => (
-  <Flex align="center" p={3} pt={4} pb={0}>
+  <Flex justify="center" align="center" p={3} pt={4} pb={0}>
     <Icon
       name="remove_red_eye"
       size={16}
