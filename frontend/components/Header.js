4546134b944ea0a4b319d729cfcc80b25821e590
Lachlan Campbell <lachlan.campbell@icloud.com>
2017-09-09 15:07:55 -0400

    Bigger Heading

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -4,23 +4,17 @@ import styled, { keyframes } from 'styled-components'
 import colors from './colors'
 import Icon from './Icon'
 
-const Name = styled(Heading)`
-  font-size: 1.25rem;
-  text-transform: uppercase;
-  letter-spacing: 0.15em;
-  color: ${colors.grey};
-`
-
 const Header = ({ status }) => (
   <Flex justify="center" align="center" p={3} pt={4} pb={0}>
     <Icon
       name="remove_red_eye"
-      size={16}
+      size={32}
+      mr={2}
       fill={status ? colors.blue : colors.grey}
     />
-    <Name m={0} ml={1}>
+    <Heading f={6} m={0}>
       Blink
-    </Name>
+    </Heading>
   </Flex>
 )
 
