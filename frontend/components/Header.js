32efac677ee6ee29fcb932ce33f97389fed7f813
Lachlan Campbell <lachlan.campbell@icloud.com>
2017-09-09 10:08:06 -0400

    Change color on status

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -6,7 +6,7 @@ import colors from './colors'
 const Name = styled(Heading)`
   font-size: 1.25rem;
   text-transform: uppercase;
-  letter-spacing: .15em;
+  letter-spacing: 0.15em;
   color: ${colors.grey};
 `
 
@@ -17,17 +17,19 @@ const bulge = keyframes`
 `
 const Badge = styled.span`
   display: inline-block;
-  width: .75rem;
-  height: .75rem;
+  width: 0.75rem;
+  height: 0.75rem;
   border-radius: 100%;
-  background-color: ${colors.red};
+  background-color: ${props => (props.status ? colors.red : colors.grey)};
   animation: ${bulge} 2s ease-in-out infinite;
 `
 
-const Header = () => (
+const Header = ({ status }) => (
   <Flex align="center" py={3}>
-    <Badge />
-    <Name m={0} ml={1}>Blink</Name>
+    <Badge status={status} />
+    <Name m={0} ml={1}>
+      Blink
+    </Name>
   </Flex>
 )
 
