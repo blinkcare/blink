1e81566c72a5827c7448270726c56a68f5957065
Lachlan Campbell <lachlan.campbell@icloud.com>
2017-09-08 22:34:15 -0400

    Stateful Body

--- frontend/components/Body.js
+++ frontend/components/Body.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Box, Flex, Text } from 'rebass'
 
-const Body = () => (
-  <Box>
-    <Text>Hello!</Text>
-  </Box>
-)
+class Body extends Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <Box>
+        <Text>Hello!</Text>
+      </Box>
+    )
+  }
+}
 
 export default Body
