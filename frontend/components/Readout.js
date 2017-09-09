ab8a2fff823515a5276465a24566bf8150b1cde9
Lachlan Campbell <lachlan.campbell@icloud.com>
2017-09-09 17:50:59 -0400

    Break text

--- frontend/components/Readout.js
+++ frontend/components/Readout.js
@@ -7,6 +7,8 @@ const Base = styled(Text)`
   color: ${colors.black};
   font-size: 6rem;
   font-weight: 500;
+  line-height: 1.25;
+  word-break: break-all;
 `
 
 const Readout = props => <Base {...props} />
